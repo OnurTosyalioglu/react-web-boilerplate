b20c610fdec298563e884a59d5c0b6692534434e
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-10-04 00:57:55 +0300

    [DEV] bootstrap theme, redux, localization

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 
+import { Provider } from 'react-redux'
 import { I18nextProvider } from 'react-i18next'
-import i18n from './locale/i18n'
-import Main from './page/landing/main/Main'
 import { ThemeProvider } from '@material-ui/core'
-import theme from './style/theme'
 
-const App = () => {  
-	return <Fragment>
-		<I18nextProvider i18n={i18n}>
-			<ThemeProvider theme={theme}>
-				<Main />
-			</ThemeProvider>
-		</I18nextProvider>
-	</Fragment>
+import store from './redux/store/index'
+import i18n from './locale/i18n'
+import theme from './style/theme'
+import Main from './page/landing/main/Main'
+
+const App = () => {
+  return <Fragment>
+		<Provider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider theme={theme}>
+          <Main />
+        </ThemeProvider>
+      </I18nextProvider>
+		</Provider>
+  </Fragment>
 }
 
 export default App
