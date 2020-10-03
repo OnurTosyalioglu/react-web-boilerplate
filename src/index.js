0aa7581f3d6cf1873c545f3d31a414c77887c3e9
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-10-04 02:28:54 +0300

    [DEV] entrypoint refactor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import store from './redux/store/index.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(
-	<Provider store={store}>
-  	<App />
-	</Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'))
 
-serviceWorker.unregister();
+serviceWorker.unregister()
