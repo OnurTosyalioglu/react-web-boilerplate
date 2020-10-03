624e7b0605c897b07154859dce5359d17a634c29
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-10-04 01:04:19 +0300

    [DEV] theme bootstrapping

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,5 +1,12 @@
 import { createMuiTheme } from '@material-ui/core'
+import { indigo } from '@material-ui/core/colors'
 
-const theme = createMuiTheme({})
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: indigo[500],
+    },
+  },
+})
 
 export default theme
