b68252901e92e6799efc8c8ee4037455144ecc72
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-10-13 09:10:52 +0300

    [DEV] tab integration

--- src/page/profile/profile/Profile.js
+++ src/page/profile/profile/Profile.js
@@ -1,8 +1,4 @@
-import React, { 
-	Fragment,
-	useState,
-	useEffect,
-} from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { withStyles } from '@material-ui/core/styles'
 
@@ -10,21 +6,69 @@ import styles from './styles'
 import props from './props'
 
 import {
-
+  Container,
+  Grid,
+  Tabs,
+  Tab,
 } from '@material-ui/core'
 
+import Account from '../account/Account'
+import Information from '../information/Information'
+
+import Header from '../../../component/profile/header/Header'
+
 const Profile = (props) => {
-	const { t, i18n } = useTranslation()
-	const { classes } = props
+  const { t, i18n } = useTranslation()
+  const { classes } = props
+  const [index, setIndex] = useState(0)
 
-	useEffect(() => {  })
+  useEffect(() => {})
 
-	return <Fragment>
-		
-	</Fragment>
+  const tabHandler = (e, index) => setIndex(index)
+
+  return (
+    <Fragment>
+      <Container maxWidth={'sm'}>
+        <Grid container>
+          <Header />
+
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}>
+            <Tabs
+              variant={'fullWidth'}
+              onChange={tabHandler}
+              value={index}>
+              <Tab
+                label={t('information')}
+                className={classes.tab}
+              />
+              <Tab
+                label={t('account')}
+                className={classes.tab}
+              />
+            </Tabs>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}>
+              {index === 0 && <Information />}
+              {index === 1 && <Account />}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </Fragment>
+  )
 }
 
 Profile.propTypes = props
 
 export default withStyles(styles)(Profile)
-
