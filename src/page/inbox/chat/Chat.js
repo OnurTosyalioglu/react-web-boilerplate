aaebd7d8f0c89ee767c1dd22e8938ee3ebd5df3a
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-10-13 09:11:16 +0300

    [DEV] tab integration

--- src/page/inbox/chat/Chat.js
+++ src/page/inbox/chat/Chat.js
@@ -1,8 +1,4 @@
-import React, { 
-	Fragment,
-	useState,
-	useEffect,
-} from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { withStyles } from '@material-ui/core/styles'
 
@@ -10,21 +6,65 @@ import styles from './styles'
 import props from './props'
 
 import {
-
+  Container,
+  Grid,
+  Tabs,
+  Tab,
 } from '@material-ui/core'
 
+import Messages from '../messages/Messages'
+import Notifications from '../notifications/Notifications'
+
 const Chat = (props) => {
-	const { t, i18n } = useTranslation()
-	const { classes } = props
+  const { t, i18n } = useTranslation()
+  const { classes } = props
+  const [index, setIndex] = useState(0)
 
-	useEffect(() => {  })
+  useEffect(() => {})
 
-	return <Fragment>
-		
-	</Fragment>
+  const tabHandler = (e, index) => setIndex(index)
+
+  return (
+    <Fragment>
+      <Container maxWidth={'sm'}>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}>
+            <Tabs
+              variant={'fullWidth'}
+              onChange={tabHandler}
+              value={index}>
+              <Tab
+                label={t('messages')}
+                className={classes.tab}
+              />
+              <Tab
+                label={t('notifications')}
+                className={classes.tab}
+              />
+            </Tabs>
+            <Grid
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+              xl={12}>
+              {index === 0 && <Messages />}
+              {index === 1 && <Notifications />}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </Fragment>
+  )
 }
 
 Chat.propTypes = props
 
 export default withStyles(styles)(Chat)
-
