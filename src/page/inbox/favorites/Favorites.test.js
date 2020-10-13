4620849384648b535181d9a0e6424b5cebc60ba9
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-10-13 05:02:02 +0300

    [DEV] refactor domains

---
+++ src/page/inbox/favorites/Favorites.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Favorites from './Favorites'
+
+test('Favorites component test', () => {
+  const Render = render(<Favorites />)
+
+	expect(true).toBe(true)
+})
