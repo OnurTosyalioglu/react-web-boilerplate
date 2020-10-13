6048648a4977b12451e9fd77bf2f84d78ecbda79
onurtosyalioglu <onurtosyalioglu@gmail.com>
2020-10-13 04:55:07 +0300

    [DEV] refactor domains

---
+++ src/page/search/search/Search.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Search from './Search'
+
+test('Search component test', () => {
+  const Render = render(<Search />)
+
+	expect(true).toBe(true)
+})
---
+++ src/page/share/share/Share.test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import ShareDetail from './ShareDetail'
+
+test('ShareDetail component test', () => {
+  const Render = render(<ShareDetail />)
+
+	expect(true).toBe(true)
+})
